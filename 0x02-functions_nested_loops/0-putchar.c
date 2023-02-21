8f517d3e6594da2a286925707c0fbd742d9288cc
Olapade Naheem <naheemoalapde@gmail.com>
2023-02-21 06:30:08 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 0)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchat('\n');
+	return (0);
+}
