39e05173f120c3f84e26cc907597a1b475750b48
Olapade Naheem <naheemoalapde@gmail.com>
2023-02-21 06:42:28 -0800

    Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Return: 0
+ * Return:0
  */
 
 int main(void)
@@ -10,11 +10,12 @@ int main(void)
 	char c[9] = "_putchar";
 	int i = 0;
 
-	while (i < 0)
+	while (i < 8)
 	{
 		_putchar(c[i]);
 		i++;
 	}
-	_putchat('\n');
+
+	_putchar('\n');
 	return (0);
 }
